92760a14df93dce9f0b2c068e86cacda5d20bf16
Eugene Kubesh <e.kubesh@wis.eu.com>
2020-03-12 14:00:17 +0300

    S-T903 add exports for directives and components

--- src/public_api.ts
+++ src/public_api.ts
@@ -5,3 +5,6 @@
 // Modules
 export { FsAutocompleteChipsModule } from './app/fs-autocomplete-chips.module';
 
+export { FsAutocompleteChipsComponent } from './app/components/autocomplete-chips/autocomplete-chips.component';
+export { FsAutocompleteObjectDirective } from './app/directives/autocomplete-object/autocomplete-object.directive';
+export { FsAutocompleteChipsStaticDirective } from './app/directives/static-template/static-template.directive';
