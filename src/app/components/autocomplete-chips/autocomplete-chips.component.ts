ff494296746bc95a20a88eadf891f411e0757a59
Deployment Manager <sysadmin@firestitch.com>
2024-10-05 11:53:42 +0000

    Angular 17 Upgrade

--- src/app/components/autocomplete-chips/autocomplete-chips.component.ts
+++ src/app/components/autocomplete-chips/autocomplete-chips.component.ts
@@ -496,7 +496,7 @@ implements OnInit, OnDestroy, ControlValueAccessor, AfterViewInit {
   }
 
   public ngOnDestroy(): void {
-    this._destroy$.next();
+    this._destroy$.next(null);
     this._destroy$.complete();
   }
 
