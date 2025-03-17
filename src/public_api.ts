af91d6074c79bf356071e935e578e5db500a2c18
Raymond Gigliotti <ray@firestitch.com>
2025-03-17 10:29:26 -0400

    ERROR TypeError: Cannot read properties of undefined (reading 'ɵcmp')

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,12 +1,12 @@
 export { FsAutocompleteChipsModule } from './app/fs-autocomplete-chips.module';
 
 export { FsAutocompleteChipsComponent } from './app/components/autocomplete-chips/autocomplete-chips.component';
-export { FsAutocompleteChipsTextValidIndicatorDirective } from './app/directives';
 export { FsAutocompleteChipsNoResultsDirective } from './app/directives/autocomplete-chips-no-results.directive';
 export { FsAutocompleteChipsSubtemplateDirective } from './app/directives/autocomplete-chips-subtemplate.directive';
 export { FsAutocompleteChipsTemplateDirective } from './app/directives/autocomplete-chips-template.directive';
 export { FsAutocompleteChipsSuffixDirective } from './app/directives/chips-suffix.directive';
 export { FsAutocompleteChipSelectedSuffixDirective } from './app/directives/selected-chip-suffix.directive';
 export { FsAutocompleteChipsStaticDirective } from './app/directives/static-template.directive';
+export { FsAutocompleteChipsTextValidIndicatorDirective } from './app/directives/text-valid-indicator.directive';
 
 
